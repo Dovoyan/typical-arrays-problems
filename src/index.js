f46e9f2cf9a0009942b1b78dfcbe0ffcc102e473
Dovoyan <yankavalenkau@gmail.com>
2020-10-03 19:05:25 +0300

    feat: did the task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 
 exports.min = function min (array) {
-  return 0;
+
+  if (!array || array.length == 0) return 0;
+  let x = 9999999999999999999999999999999;
+  for (let i = 0;i < array.length; i++){
+    if (array[i]<x)x=array[i]
+  }
+  return x;
+  
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array || array.length == 0) return 0;
+  let x = -9999999999999999999999999999999;
+  for (let i = 0; i < array.length; i++) {
+      if (array[i] > x) x = array[i];
+  }
+  return x;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array || array.length == 0 ) return 0;
+  let x = 0;
+  for (let i = 0; i < array.length; i++) {
+       x += array[i];
+  }
+  x = x / array.length;
+  return x;
 }
